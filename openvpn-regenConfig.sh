c0588602da08c1165b4d53923b29a5e8efb403c7
Hexidaz <35564885+Hexidaz@users.noreply.github.com>
2024-08-13 12:15:39 +0700

    Add regenerate client config

---
+++ openvpn-regenConfig.sh
@@ -0,0 +1,207 @@
+#!/bin/bash
+# shellcheck disable=SC1091,SC2164,SC2034,SC1072,SC1073,SC1009
+
+# Secure OpenVPN server installer for Debian, Ubuntu, CentOS, Amazon Linux 2, Fedora, Oracle Linux 8, Arch Linux, Rocky Linux and AlmaLinux.
+# https://github.com/angristan/openvpn-install
+
+function isRoot() {
+	if [ "$EUID" -ne 0 ]; then
+		return 1
+	fi
+}
+
+function tunAvailable() {
+	if [ ! -e /dev/net/tun ]; then
+		return 1
+	fi
+}
+
+function checkOS() {
+	if [[ -e /etc/debian_version ]]; then
+		OS="debian"
+		source /etc/os-release
+
+		if [[ $ID == "debian" || $ID == "raspbian" ]]; then
+			if [[ $VERSION_ID -lt 9 ]]; then
+				echo "⚠️ Your version of Debian is not supported."
+				echo ""
+				echo "However, if you're using Debian >= 9 or unstable/testing then you can continue, at your own risk."
+				echo ""
+				until [[ $CONTINUE =~ (y|n) ]]; do
+					read -rp "Continue? [y/n]: " -e CONTINUE
+				done
+				if [[ $CONTINUE == "n" ]]; then
+					exit 1
+				fi
+			fi
+		elif [[ $ID == "ubuntu" ]]; then
+			OS="ubuntu"
+			MAJOR_UBUNTU_VERSION=$(echo "$VERSION_ID" | cut -d '.' -f1)
+			if [[ $MAJOR_UBUNTU_VERSION -lt 16 ]]; then
+				echo "⚠️ Your version of Ubuntu is not supported."
+				echo ""
+				echo "However, if you're using Ubuntu >= 16.04 or beta, then you can continue, at your own risk."
+				echo ""
+				until [[ $CONTINUE =~ (y|n) ]]; do
+					read -rp "Continue? [y/n]: " -e CONTINUE
+				done
+				if [[ $CONTINUE == "n" ]]; then
+					exit 1
+				fi
+			fi
+		fi
+	elif [[ -e /etc/system-release ]]; then
+		source /etc/os-release
+		if [[ $ID == "fedora" || $ID_LIKE == "fedora" ]]; then
+			OS="fedora"
+		fi
+		if [[ $ID == "centos" || $ID == "rocky" || $ID == "almalinux" ]]; then
+			OS="centos"
+			if [[ ${VERSION_ID%.*} -lt 7 ]]; then
+				echo "⚠️ Your version of CentOS is not supported."
+				echo ""
+				echo "The script only support CentOS 7 and CentOS 8."
+				echo ""
+				exit 1
+			fi
+		fi
+		if [[ $ID == "ol" ]]; then
+			OS="oracle"
+			if [[ ! $VERSION_ID =~ (8) ]]; then
+				echo "Your version of Oracle Linux is not supported."
+				echo ""
+				echo "The script only support Oracle Linux 8."
+				exit 1
+			fi
+		fi
+		if [[ $ID == "amzn" ]]; then
+			OS="amzn"
+			if [[ $VERSION_ID != "2" ]]; then
+				echo "⚠️ Your version of Amazon Linux is not supported."
+				echo ""
+				echo "The script only support Amazon Linux 2."
+				echo ""
+				exit 1
+			fi
+		fi
+	elif [[ -e /etc/arch-release ]]; then
+		OS=arch
+	else
+		echo "Looks like you aren't running this installer on a Debian, Ubuntu, Fedora, CentOS, Amazon Linux 2, Oracle Linux 8 or Arch Linux system"
+		exit 1
+	fi
+}
+
+function initialCheck() {
+	if ! isRoot; then
+		echo "Sorry, you need to run this as root"
+		exit 1
+	fi
+	if ! tunAvailable; then
+		echo "TUN is not available"
+		exit 1
+	fi
+	checkOS
+}
+
+function regenConfig() {
+	# echo ""
+	# echo "Tell me a name for the client."
+	# echo "The name must consist of alphanumeric character. It may also include an underscore or a dash."
+
+	# until [[ $CLIENT =~ ^[a-zA-Z0-9_-]+$ ]]; do
+	# 	read -rp "Client name: " -e CLIENT
+	# done
+	# Client Name
+	CLIENT=$1
+
+	# echo ""
+	# echo "Do you want to protect the configuration file with a password?"
+	# echo "(e.g. encrypt the private key with a password)"
+	# echo "   1) Add a passwordless client"
+	# echo "   2) Use a password for the client"
+
+	# until [[ $PASS =~ ^[1-2]$ ]]; do
+	# 	read -rp "Select an option [1-2]: " -e -i 1 PASS
+	# done
+	PASS=1
+
+	CLIENTEXISTS=$(tail -n +2 /etc/openvpn/easy-rsa/pki/index.txt | grep -c -E "/CN=$CLIENT\$")
+	if [[ $CLIENTEXISTS == '1' ]]; then
+
+		# Home directory of the user, where the client configuration will be written
+		# if [ -e "/home/${CLIENT}" ]; then
+		# 	# if $1 is a user name
+		# 	homeDir="/home/${CLIENT}"
+		# elif [ "${SUDO_USER}" ]; then
+		# 	# if not, use SUDO_USER
+		# 	if [ "${SUDO_USER}" == "root" ]; then
+		# 		# If running sudo as root
+		# 		homeDir="/root"
+		# 	else
+		# 		homeDir="/home/${SUDO_USER}"
+		# 	fi
+		# else
+		# 	# if not SUDO_USER, use /root
+		# 	homeDir="/root"
+		# fi
+		homeDir=$2
+
+		# Determine if we use tls-auth or tls-crypt
+		if grep -qs "^tls-crypt" /etc/openvpn/server.conf; then
+			TLS_SIG="1"
+		elif grep -qs "^tls-auth" /etc/openvpn/server.conf; then
+			TLS_SIG="2"
+		fi
+
+		# Generates the custom client.ovpn
+		cp /etc/openvpn/client-template.txt "$homeDir/$CLIENT.ovpn"
+		{
+			echo "<ca>"
+			cat "/etc/openvpn/easy-rsa/pki/ca.crt"
+			echo "</ca>"
+
+			echo "<cert>"
+			awk '/BEGIN/,/END CERTIFICATE/' "/etc/openvpn/easy-rsa/pki/issued/$CLIENT.crt"
+			echo "</cert>"
+
+			echo "<key>"
+			cat "/etc/openvpn/easy-rsa/pki/private/$CLIENT.key"
+			echo "</key>"
+
+			case $TLS_SIG in
+			1)
+				echo "<tls-crypt>"
+				cat /etc/openvpn/tls-crypt.key
+				echo "</tls-crypt>"
+				;;
+			2)
+				echo "key-direction 1"
+				echo "<tls-auth>"
+				cat /etc/openvpn/tls-auth.key
+				echo "</tls-auth>"
+				;;
+			esac
+		} >>"$homeDir/$CLIENT.ovpn"
+
+		echo ""
+		echo "The configuration file has been written to $homeDir/$CLIENT.ovpn."
+		echo "Download the .ovpn file and import it in your OpenVPN client."
+	else
+		echo "Client not found"
+	fi
+
+	exit 0
+}
+
+# Check for root, TUN, OS...
+initialCheck
+
+# # Check if OpenVPN is already installed
+# if [[ -e /etc/openvpn/server.conf && $AUTO_INSTALL != "y" ]]; then
+# 	manageMenu
+# else
+# 	installOpenVPN
+# fi
+ovpnFilePath=$(pwd)
+regenConfig $1 $ovpnFilePath
